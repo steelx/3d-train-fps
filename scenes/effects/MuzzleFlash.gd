dfcb6964963bc8342948878490f351e5f1bea9b6
steelx <ajinkyax@pm.me>
2023-07-21 17:06:24 +0530

    fix: better weapon Muzzle fire

--- scenes/effects/MuzzleFlash.gd
+++ scenes/effects/MuzzleFlash.gd
@@ -1,10 +1,23 @@
 extends Node3D
 
+# display any one MuzzleFlash model from $muzzleflash1 to $muzzleflash3
+@onready var muzzle1 := $muzzleflash1
+@onready var muzzle2 := $muzzleflash2
+@onready var muzzle3 := $muzzleflash3
+
+var MUZZLE_VARIANTS: Array
+
+@export var muzzle_variant: int = 1:
+	set(val):
+		if val > 3 or val < 1:
+			muzzle_variant = 1
+
 @export var flash_time := 0.05
 var timer: Timer
 
 
 func _ready() -> void:
+	MUZZLE_VARIANTS = [muzzle1, muzzle2, muzzle3]
 	timer = Timer.new()
 	timer.set_wait_time(flash_time)
 	timer.set_one_shot(true)
@@ -13,10 +26,20 @@ func _ready() -> void:
 	hide()
 
 
+func toggle_muzzle_variant() -> void:
+	muzzle_variant = (muzzle_variant % 3) - 1
+	var muzzle: Node3D = MUZZLE_VARIANTS[muzzle_variant]
+	muzzle1.hide()
+	muzzle2.hide()
+	muzzle3.hide()
+	muzzle.show()
+
+
 func flash() -> void:
 	timer.start()
 	rotation.z = randf_range(0, 2 * PI)
 	show()
+	toggle_muzzle_variant()
 
 
 func on_flash_timeout() -> void:
@@ -24,4 +47,12 @@ func on_flash_timeout() -> void:
 
 
 func _on_machine_gun_e_fired(_bullet_emitter: Node3D) -> void:
+	self.flash()
+
+
+func _on_shotgun_e_fired(_bullet_emitter: Node3D) -> void:
+	self.flash()
+
+
+func _on_rocket_launcher_e_fired(_bullet_emitter: Node3D) -> void:
 	self.flash()
