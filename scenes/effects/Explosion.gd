235dce214ff1bc25875bb12e08d62ae68a8f2e20
steelx <ajinkyax@pm.me>
2023-07-22 11:39:10 +0530

    feat: Explosion

---
+++ scenes/effects/Explosion.gd
@@ -0,0 +1,31 @@
+extends Area3D
+
+@export var damage: int = 70
+@onready var partcle1 := $GPUParticles3D
+@onready var partcle2 := $GPUParticles3D2
+@onready var collisionShape := $CollisionShape3D
+
+
+func explode() -> void:
+	partcle1.emitting = true
+	partcle2.emitting = true
+	var query := PhysicsShapeQueryParameters3D.new()
+	query.set_transform(global_transform)
+	query.set_shape(collisionShape.shape)
+	query.collision_mask = self.collision_mask
+	var results := get_world_3d().direct_space_state.intersect_shape(query)
+	for result in results:
+		if result.collider.has_method("hurt"):
+			result.collider.hurt(
+				damage, global_transform.origin.direction_to(result.collider.global_transform)
+			)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass  # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
